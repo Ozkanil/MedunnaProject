1f87921f207737e0b09c399ca4321b61318af8ce
Ozkanil <ozkanilhan@gmail.com>
2022-02-28 00:21:39 +0300

    First commit

---
+++ src/test/java/Hooks/Hooks.java
@@ -0,0 +1,28 @@
+package Hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+    @Before
+    public void setUp(){
+
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+
+        if (scenario.isFailed()) {
+            final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+
+    }
+
+}
---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,20 @@
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
+        features = "./src/test/resources/features",
+        glue = {"stepdefinitions","Hooks"},
+        tags = "@excel_login",
+        dryRun = false
+)
+public class Runner {
+}
