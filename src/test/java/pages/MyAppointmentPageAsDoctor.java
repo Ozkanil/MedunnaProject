90733305269cb8658eb03823cfc1fffeaaa4e1f0
Arif Canpol <canpolska@gmail.com>
2022-03-05 21:59:18 +0100

    MyAppointmentPageAsDoctor commit

--- src/test/java/pages/MyAppointmentPageAsDoctor.java
+++ src/test/java/pages/MyAppointmentPageAsDoctor.java
@@ -23,50 +23,106 @@ public class MyAppointmentPageAsDoctor {
     @FindBy (id="toDate")
     public WebElement toDateAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[1])")
+    @FindBy(xpath = "(//*[tr])[1]")
+    public WebElement headersOfAppointmentsTable;
+
+    @FindBy(xpath = "(//*[tr])[2]")
+    public WebElement allDataOfAppointmentsTable;
+
+    @FindBy(xpath = "(//*[td])[1]")
+    public WebElement firstRowOfAppointmentsTable;
+
+    @FindBy(xpath = "(//*[td])[2])")
+    public WebElement secondRowOfAppointmentsTable;
+
+    @FindBy(xpath = "(//*[td])[3])")
+    public WebElement thirdRowOfAppointmentsList;
+
+    @FindBy(xpath = "(//*[td])[4])")
+    public WebElement forthRowOfAppointmentsList;
+
+    @FindBy(xpath = "(//*[td])[5])")
+    public WebElement fifthRowOfOfAppointmentsList;
+
+    @FindBy(xpath = "(//td[.])[1]")
     public WebElement patientIdOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[2])")
+    @FindBy(xpath = "(//td[span])[1]")
     public WebElement startDateOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[3])")
+    @FindBy(xpath = "(//td[span])[2]")
     public WebElement endDateOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[4])")
+    @FindBy(xpath = "(//td[span])[3]")
     public WebElement statusOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[5])")
+    @FindBy(xpath = "(//td[.])[5]")
     public WebElement anamnesisOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[6])")
+    @FindBy(xpath = "(//td[.])[6]")
     public WebElement treatmentOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[7])")
+    @FindBy(xpath = "(//td[.])[7]")
     public WebElement diagnosisOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[8])")
+    @FindBy(xpath = "(//td[.])[8]")
     public WebElement prescriptionOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[9])")
+    @FindBy(xpath = "(//td[.])[9]")
     public WebElement descriptionOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[10])")
+    @FindBy(xpath = "(//td[.])[10]")
     public WebElement createdDateOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[11])")
-    public WebElement physicianOfFirstAppointment;
+    @FindBy(xpath = "(//td[a])[1]")
+    public WebElement physicianDetailElementOfFirstAppointment;
 
-    @FindBy(xpath = "(//tbody/tr/td)[12])")
-    public WebElement patientNameOfFirstAppointment;
-
-    @FindBy (xpath = "(//td/a)[1]")
-    public WebElement clickPhysicianDetailElementOfFirstAppointment;
-
-    @FindBy (xpath = "(//td/a)[2]")
-    public WebElement clickPatientDetailElementOfFirstAppointment;
+    @FindBy (xpath = "(//td[a])[2]")
+    public WebElement patientDetailElementOfFirstAppointment;
 
     @FindBy(xpath = "(//*[span='Edit'])[1]" )
-    public WebElement editAppointment;
+    public WebElement editFirstAppointment;
+
+    @FindBy(xpath = "(//th[span])[1]")
+    public WebElement patientIdHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[2]")
+    public WebElement endDateHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[3]")
+    public WebElement statusHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[4]")
+    public WebElement startDateHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[5]")
+    public WebElement anamnesisHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[6]")
+    public WebElement treatmentHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[7]")
+    public WebElement diagnosisHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[8]")
+    public WebElement prescriptionHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[9]")
+    public WebElement descriptionHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[10]")
+    public WebElement createdDateHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "(//th[span])[11]")
+    public WebElement physicianNameHeaderOfAppointmentsTable;
+
+    @FindBy (xpath = "(//th[span])[12]")
+    public WebElement patientNameHeaderOfAppointmentsTable;
+
+    @FindBy(xpath = "//*[.='Days between begin date and end date must be max 15 days']")
+    public WebElement alertMessage;
+
+
 
 
 
--- src/test/java/stepdefinitions/MyAppointmentsStepdefs.java
+++ src/test/java/stepdefinitions/MyAppointmentsStepdefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 import pages.CommonPage;
 import pages.MyAppointmentPageAsDoctor;
 import pages.SignInPage;
@@ -14,35 +15,48 @@ import utilities.Driver;
 import java.time.Duration;
 
 public class MyAppointmentsStepdefs {
+    WebDriver driver;
     CommonPage commonPage=new CommonPage();
     SignInPage signInPage=new SignInPage();
     MyAppointmentPageAsDoctor myAppointmentPageAsDoctor=new MyAppointmentPageAsDoctor();
-
-    @Given("user sign in as a {string} providing {string} and {string}")
-    public void userSignInAsAProvidingAnd(String arg0, String arg1, String arg2) {
+    @Given("user sign in as a {string}")
+    public void userSignInAsA(String arg0) {
         Driver.getDriver().get(ConfigurationReader.getProperty("medunna_sign_in_url"));
-        Driver.waitAndSendText(signInPage.username,arg1);
-        Driver.waitAndSendText(signInPage.password,arg2);
-        Driver.waitAndClick(signInPage.singInButton);
-
         switch (arg0){
             case "doctor":
+                Driver.waitAndSendText(signInPage.username,ConfigurationReader.getProperty("doctor_username"));
+                Driver.waitAndSendText(signInPage.password,ConfigurationReader.getProperty("doctor_password"));
+                Driver.waitAndClick(signInPage.singInButton);
                 Driver.waitAndClick(myAppointmentPageAsDoctor.myPagesDropdown);
                 Assert.assertTrue(myAppointmentPageAsDoctor.myAppointment.isDisplayed());
-            break;
-            case "admin":
-            break;
-            case "patient":
                 Driver.waitAndClick(myAppointmentPageAsDoctor.myPagesDropdown);
-            break;
+                break;
+            case "admin":
+                Driver.waitAndSendText(signInPage.username,ConfigurationReader.getProperty("admin_username"));
+                Driver.waitAndSendText(signInPage.password,ConfigurationReader.getProperty("admin_password"));
+                Driver.waitAndClick(signInPage.singInButton);
+                break;
+            case "patient":
+                Driver.waitAndSendText(signInPage.username,ConfigurationReader.getProperty("patient_username"));
+                Driver.waitAndSendText(signInPage.password,ConfigurationReader.getProperty("patient_password"));
+                Driver.waitAndClick(signInPage.singInButton);
+                Driver.waitAndClick(myAppointmentPageAsDoctor.myPagesDropdown);
+                break;
             case "staff":
-            break;
+                Driver.waitAndSendText(signInPage.username,ConfigurationReader.getProperty("staff_username"));
+                Driver.waitAndSendText(signInPage.password,ConfigurationReader.getProperty("staff_password"));
+                Driver.waitAndClick(signInPage.singInButton);
+                break;
             case "user":
-            break;
+                Driver.waitAndSendText(signInPage.username,ConfigurationReader.getProperty("user_username"));
+                Driver.waitAndSendText(signInPage.password,ConfigurationReader.getProperty("user_password"));
+                Driver.waitAndClick(signInPage.singInButton);
+                break;
             default:
-            break;
-        }
+                System.out.println("No such user found");
+                break;
 
+    }
 
     }
 
@@ -53,22 +67,40 @@ public class MyAppointmentsStepdefs {
 
     @And("user selects My Appointments item and clicks")
     public void userSelectsMyAppointmentsItemAndClicks() {
-        Driver.waitAndClick(myAppointmentPageAsDoctor.);
+        Driver.waitAndClick(myAppointmentPageAsDoctor.myAppointment);
     }
 
     @When("user on the Appointments page")
     public void userOnTheAppointmentsPage() {
+        Driver.waitForVisibility(myAppointmentPageAsDoctor.appointmentHeading,1);
+        Assert.assertTrue(myAppointmentPageAsDoctor.appointmentHeading.isDisplayed());
     }
 
     @Then("user should see list of appointments and time slots")
     public void userShouldSeeListOfAppointmentsAndTimeSlots() {
+        Assert.assertTrue(Driver.waitForVisibility(myAppointmentPageAsDoctor.toDateAppointment,1).isDisplayed());
+        Assert.assertTrue(Driver.waitForVisibility(myAppointmentPageAsDoctor.fromDateAppointment,1).isDisplayed());
+        Assert.assertTrue(Driver.waitForVisibility(myAppointmentPageAsDoctor.allDataOfAppointmentsTable,1).isDisplayed());
     }
 
     @Then("user should see {string} information of the appointment list")
     public void userShouldSeeInformationOfTheAppointmentList(String arg0) {
+        Assert.assertTrue(Driver.waitAndGetText(myAppointmentPageAsDoctor.headersOfAppointmentsTable,1).contains(arg0));
     }
 
-    @Then("user closes the application")
-    public void userClosesTheApplication() {
+    @And("user selects time slots providing {string} and {string}")
+    public void userSelectsTimeSlotsProvidingAnd(String arg0, String arg1) {
+        Driver.waitAndSendText(myAppointmentPageAsDoctor.fromDateAppointment, arg0);
+        Driver.waitAndSendText(myAppointmentPageAsDoctor.toDateAppointment, arg1);
+        Driver.wait(1);
+        try {
+            System.out.println(Driver.waitAndGetText(myAppointmentPageAsDoctor.alertMessage,1));
+        } catch (Exception e) {
+        }
+    }
+
+    @Then("user cliks on the edit button")
+    public void userCliksOnTheEditButton() {
+        Driver.waitAndClick(myAppointmentPageAsDoctor.editFirstAppointment);
     }
 }
