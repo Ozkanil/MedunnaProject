1f029b6f5bfdf8ff825155b9536027e148e43bc5
Arif Canpol <canpolska@gmail.com>
2022-03-12 14:03:40 +0100

    Project latest version

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = "./src/test/resources/features",
-        glue = {"stepdefinitions"},
+        glue = {"stepdefinitions","hooks"},
         tags = "@smoke",
         dryRun = false
 )
