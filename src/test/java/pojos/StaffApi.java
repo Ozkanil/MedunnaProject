69b6c70db4ac86f75dd5f463852ec170f1b1c11d
farzu <95378751+fatiharzu@users.noreply.github.com>
2022-03-19 16:09:38 +0100

    Project final version

---
+++ src/test/java/pojos/StaffApi.java
@@ -0,0 +1,166 @@
+package pojos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StaffApi {
+
+     /*   {
+        "createdBy": "eymenadmin",
+        "createdDate": "2022-03-08T15:49:18.661268Z",
+        "id": 15503,
+        "firstName": "eymen",
+        "lastName": "arzu",
+        "birthDate": "2022-03-07T23:00:00Z",
+        "phone": "123-123-1234",
+        "gender": "MALE",
+        "bloodGroup": "Apositive",
+        "adress": "Yeni mah 214 sok .NO 15",
+        "description": "Psychothrepist",
+        "user": {*/
+
+    private String createdBy;
+    private String createdDate;
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String phone;
+    private String gender;
+    private String bloodGroup;
+    private String adress;
+    private String description;
+    private String user;
+
+    public StaffApi() {
+    }
+
+    public StaffApi(String createdBy, String createdDate, String id, String firstName, String lastName, String birthDate, String phone, String gender, String bloodGroup, String adress, String description, String user) {
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.gender = gender;
+        this.bloodGroup = bloodGroup;
+        this.adress = adress;
+        this.description = description;
+        this.user = user;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "StaffApi{" +
+                "createdBy='" + createdBy + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                ", bloodGroup='" + bloodGroup + '\'' +
+                ", adress='" + adress + '\'' +
+                ", description='" + description + '\'' +
+                ", user='" + user + '\'' +
+                '}';
+    }
+}
